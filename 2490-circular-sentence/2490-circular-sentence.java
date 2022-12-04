6b6c2bdea6be9345c5ed924a83a0190fd44d6110
Kapil0187 <90748985+Kapil0187@users.noreply.github.com>
2022-12-04 11:20:02 +0530

    Time: 3 ms (60.00%), Space: 42.2 MB (100.00%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        for(int i=0;i<sentence.length();i++)
+        {
+            char ch = sentence.charAt(i);
+            if(ch==' ')
+            {
+               if(sentence.charAt(i-1)!=sentence.charAt(i+1))
+                   return false;
+            }
+        }
+        if(sentence.charAt(0)==sentence.charAt(sentence.length()-1))
+            return true;
+        return false;
+    }
+}
