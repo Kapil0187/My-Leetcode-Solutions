182ac187675fdf14c3dc3fed1d9f87899b224464
Kapil0187 <90748985+Kapil0187@users.noreply.github.com>
2023-02-22 11:22:31 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = S[j].charAt(0);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.numIslands(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+class Pair{
+    int row;
+    int col;
+    Pair(int row,int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+}
+class Solution {
+    // Function to find the number of islands.
+    public int numIslands(char[][] grid) {
+        // Code here
+        int r = grid.length;
+        int c = grid[0].length;
+        
+        int vis[][] = new int[r][c];
+        int count = 0;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]=='1' && vis[i][j]==0)
+                {
+                    bfs(grid,vis,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void bfs(char[][] grid,int vis[][],int r,int c)
+    {
+        vis[r][c] = 1;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(r,c));
+        while(!q.isEmpty())
+        {
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.poll();
+            for(int dr=-1;dr<=1;dr++)
+            {
+                for(int dc=-1;dc<=1;dc++)
+                {
+                    int drow = row+dr;
+                    int dcol = col+dc;
+                    if(drow>=0 && dcol>=0 && drow<grid.length && dcol<grid[0].length && grid[drow][dcol]=='1' && vis[drow][dcol]==0)
+                    {
+                        q.add(new Pair(drow,dcol));
+                        vis[drow][dcol]=1;
+                    }
+                }
+            }
+        }
+    }
+}
