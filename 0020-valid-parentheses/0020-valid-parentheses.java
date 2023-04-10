caa0901070af52f880a71e17bc273488bf2f8d0a
Kapil0187 <90748985+Kapil0187@users.noreply.github.com>
2023-04-10 20:12:02 +0530

    Time: 2 ms (79.07%), Space: 40.8 MB (33.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack<Character>();
+      for(int i=0;i<s.length();i++)
+      {
+         if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
+             stack.push(s.charAt(i));
+         else if(!stack.isEmpty())
+         {
+             if(s.charAt(i)==')'&&(char)stack.peek()=='(')
+                 stack.pop();
+             else if(s.charAt(i)==']'&&(char)stack.peek()=='[')
+                 stack.pop();
+             else if(s.charAt(i)=='}'&&(char)stack.peek()=='{')
+                 stack.pop();
+             else
+                 return false;
+         }
+         else
+             return false;
+      }
+     if(stack.isEmpty())
+         return true;
+     return false;
+    }
+}
+      
